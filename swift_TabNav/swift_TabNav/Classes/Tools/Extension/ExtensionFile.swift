a1640f5aa669e460e96086d34d19704e9ef0e483
SpongeBob_Han <1605319671@qq.com>
2015-06-16 13:21:34 +0800

    4.4 ExtensionFile.swift，扩展类，将所有顶部导航栏底背景样式通过“runtime”运行机制，修改为蓝色。

---
+++ swift_TabNav/swift_TabNav/Classes/Tools/Extension/ExtensionFile.swift
@@ -0,0 +1,89 @@
+//
+//  ExtensionFile.swift
+//  Chatbei
+//
+//  Created by Scor on 15/5/4.
+//  Copyright (c) 2015年 Scor. All rights reserved.
+//
+
+import UIKit
+
+// 拓展
+extension UIColor {
+    
+    //主题色
+    class func applicationMainColor() -> UIColor {
+//        return UIColor(red: 238/255, green: 64/255, blue: 86/255, alpha:1)
+        return UIColor.whiteColor()
+    }
+    
+    //第二主题色
+    class func applicationSecondColor() -> UIColor {
+        return UIColor(red: 0/255, green: 155/255, blue: 255/255, alpha:1)
+//        return UIColor(patternImage: UIImage(named: "banner.png")!)
+    }
+    
+    //警告颜色
+    class func applicationWarningColor() -> UIColor {
+        return UIColor(red: 0.1, green: 1, blue: 0, alpha: 1)
+    }
+    
+    //链接颜色
+    class func applicationLinkColor() -> UIColor {
+        return UIColor(red: 59/255, green: 89/255, blue: 152/255, alpha:1)
+    }
+    
+}
+
+
+// Navagation拓展
+extension UIViewController {
+    func viewDidLoadForChangeTitleColor() {
+        self.viewDidLoadForChangeTitleColor()
+        if self.isKindOfClass(UINavigationController.classForCoder()) {
+            self.changeNavigationBarTextColor(self as! UINavigationController)
+        }
+    }
+    
+    func changeNavigationBarTextColor(navController: UINavigationController) {
+        let nav = navController as UINavigationController
+        let dic = NSDictionary(object: UIColor.applicationMainColor(),
+            forKey:NSForegroundColorAttributeName)
+        nav.navigationBar.titleTextAttributes = dic as [NSObject : AnyObject]
+        nav.navigationBar.barTintColor = UIColor.applicationSecondColor()
+        nav.navigationBar.tintColor = UIColor.applicationMainColor()
+       
+    }
+    
+}
+
+// button
+extension UIButton{
+    
+    //按钮可以样式
+    func setButtonCanUse(button : UIButton){
+        button.userInteractionEnabled = true
+        button.backgroundColor = UIColor(red: 0/255, green: 155/255, blue: 255/255, alpha:1)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 3
+    }
+    
+    //按钮不可用样式
+    func setButtonCannntUse(button: UIButton){
+        button.userInteractionEnabled = false
+        button.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius  = 3
+    }
+
+}
+
+
+extension UIView{
+    func setViewCornerRadius(view:UIView ,radius:CGFloat){
+        view.layer.cornerRadius = radius
+        view.layer.masksToBounds = true
+    }
+    
+}
+
