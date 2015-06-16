1c47618bd1e94ec663c4d06ec195b4303c664f96
SpongeBob_Han <1605319671@qq.com>
2015-06-16 13:13:12 +0800

    4.2 AppDelegate.swift

--- swift_TabNav/swift_TabNav/AppDelegate.swift
+++ swift_TabNav/swift_TabNav/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Navgation's style --> color.
+        swizzlingMethod(UIViewController.self, oldSelector: "viewDidLoad", newSelector: "viewDidLoadForChangeTitleColor")
+        
         return true
     }
 
@@ -41,6 +44,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    //Hook工具类--runtime
+    func swizzlingMethod(clzz: AnyClass, oldSelector: Selector, newSelector: Selector) {
+        let oldMethod = class_getInstanceMethod(clzz, oldSelector)
+        let newMethod = class_getInstanceMethod(clzz, newSelector)
+        method_exchangeImplementations(oldMethod, newMethod)
+    }
 }
 
